3fbd880ff158601a692d37e5d23186313007f089
taqq <62509841+taqqqq@users.noreply.github.com>
2025-03-13 12:20:54 +0200

    main

---
+++ src/components/JobListings.vue
@@ -0,0 +1,57 @@
+<script setup>
+    import JobListing from '@/components/JobListing.vue'
+    import {reactive, defineProps, onMounted} from 'vue';
+    import {RouterLink} from 'vue-router'
+    import axios from 'axios'
+
+    defineProps({
+        limit: Number,
+        showButton: {
+            type: Boolean,
+            default: false,
+        }
+    });
+
+    const state = reactive({
+        jobs: [],
+        isLoading: true
+    });
+
+    //const jobs = ref([]);
+
+    onMounted(async() => {
+        try {
+            const response = await axios.get('http://localhost:5000/jobs');
+            state.jobs = response.data;
+        }
+        catch (error){
+            console.error("Error fething data " + error);
+        }
+        finally {
+            state.isLoading = false;
+        }
+    });
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
+            </div>
+        </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <RouterLink
+        to="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</RouterLink
+      >
+    </section>
+
+    
+</template>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
+
